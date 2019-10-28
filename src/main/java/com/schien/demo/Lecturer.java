be96ff237a337b05fab7cd40a7eb346516288f4b
Daniel Schien <dance@web.de>
2019-10-28 12:48:45 +0000

    fixed db unit test

--- src/main/java/com/schien/demo/Lecturer.java
+++ src/main/java/com/schien/demo/Lecturer.java
@@ -1,9 +1,6 @@
 package com.schien.demo;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Lecturer {
@@ -12,7 +9,10 @@ public class Lecturer {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     protected Lecturer() {
