f7f92bf070ddb8d8e4adec5a0aad445c5607f3c8
Daniel Schien <dance@web.de>
2019-10-28 10:32:11 +0000

    added new test

--- src/test/java/com/schien/demo/DemoApplicationTests.java
+++ src/test/java/com/schien/demo/DemoApplicationTests.java
@@ -1,5 +1,8 @@
 package com.schien.demo;
 
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,9 +10,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import javax.transaction.Transactional;
+
+import java.util.Optional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -17,6 +27,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 //https://stackoverflow.com/questions/13364112/spring-profiles-and-testing
 @ActiveProfiles("test") // use application-test.yml properties (in-memory DB)
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
+        TransactionalTestExecutionListener.class, // use transactional test execution
+        DbUnitTestExecutionListener.class}) // to read datasets from file
+@Transactional // rollback DB in between tests
 @SpringBootTest
 public class DemoApplicationTests {
 
@@ -25,10 +39,24 @@ public class DemoApplicationTests {
     @Autowired
     private LecturerRestController lecturerRestController;
 
+
+    @Autowired
+    LecturerRepository lecturerRepository;
+
     @Before
     public void setup() {
         this.mvc = MockMvcBuilders.standaloneSetup(lecturerRestController).build();
     }
+
+    @Test
+    @DatabaseSetup("/lecturer_test.xml") // read dataset from file
+    public void basic_test() {
+        Optional<Lecturer> lecturer = lecturerRepository.findById(1L);
+        if (lecturer.isPresent()) {
+            Assert.assertTrue(lecturer.get().getFirstName().equals("Dan"));
+        }
+    }
+
 
     @Test
     public void saveLecturer() throws Exception {
